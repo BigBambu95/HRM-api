db52345449f25e3e523090ef76990f0fbcf3afe4
BigBambu <hjot@yandex.ru>
2020-08-08 19:08:26 +0500

    Добавлено Api для загрузки данных конкретного сотрудника

--- routes/api/workers.js
+++ routes/api/workers.js
@@ -12,4 +12,13 @@ router.get("/", function (req, res) {
     );
 });
 
+router.get("/:id", function(req, res) {
+  Worker
+    .findById(req.params.id)
+    .then((worker) => res.json(worker))
+    .catch((err) => 
+      res.status(404).json({ message: "Ошибка при получении данных сотрудника" })
+    )
+});
+
 module.exports = router;
