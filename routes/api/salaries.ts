1ae54697ff1d81dae9d27de98e245fabf95b1764
BigBambu95 <hjot@yandex.ru>
2022-01-03 00:21:08 +0500

    Создан роут salaries

--- routes/api/salaries.ts
+++ routes/api/salaries.ts
@@ -5,17 +5,33 @@ import Worker from '../../models/Worker';
 const router = express.Router();
 
 router.get("/", function (req, res) {
+  const { sortKey, sortValue } = req.query as any
+
   Worker
-    .find()
-    .populate('salary')
-    // .find({ "salary.month": 'July' })
-    .then((workers) => {
-      
-      res.json(workers)
+    .find({ salary: { $gt: [] }})
+    .select(['name', 'salary'])
+    .populate({
+      "path": "salary",
+      "match": { "month": "July" },
     })
-    .catch((err) =>
-      res.status(404).json({ message: "Ошибка при получении списка зарплат" })
-    );
+    .sort('')
+    .lean()
+    .exec()
+    .then((workers) => {
+      const newWorkers = workers.map((worker) => ({ 
+        ...worker, 
+        salary: worker.salary[0] as any
+      }))
+
+      res.json(newWorkers.sort((a, b) => {
+        if(sortValue === 'asc') {
+          return a.salary[sortKey] - b.salary[sortKey]
+        }
+
+        return b.salary[sortKey] - a.salary[sortKey]
+      }))
+    })
+    .catch((err) => res.status(404).json({ message: "Ошибка при получении списка зарплат" }));
 });
 
 router.get("/:id", function (req, res) {
